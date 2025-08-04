0950a4b1b7f9684b5dc982eefea5cc486d4bc8d9
zeeshan138063 <zeeshan.nu@Outlook.com>
2025-08-04 20:18:22 +0500

    task 2 - SS Field

--- library/models.py
+++ library/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date, timedelta
+
+
+# For DateTimeField:
+
+def get_default_due_date():
+    """
+    function to return the default 14 days time from today
+    """
+    return date.today() + timedelta(days=14)
+
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -39,6 +51,7 @@ class Loan(models.Model):
     book = models.ForeignKey(Book, related_name='loans', on_delete=models.CASCADE)
     member = models.ForeignKey(Member, related_name='loans', on_delete=models.CASCADE)
     loan_date = models.DateField(auto_now_add=True)
+    due_date= models.DateField(default=get_default_due_date)
     return_date = models.DateField(null=True, blank=True)
     is_returned = models.BooleanField(default=False)
 
